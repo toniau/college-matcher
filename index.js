6eca671ea452e14d33808255c5bd53ee3a20834a
ubiquitous-kumquat <izzyjs@yahoo.com>
2016-12-06 13:28:35 -0500

    OTHER added conditional statements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express		= require('express');
-var app 		= express();
-var ejs 		= require('ejs');
-//var http		= require('http');
+var express     = require('express');
+var app         = express();
+var ejs         = require('ejs');
+//var http      = require('http');
 
 //var server = http.createServer(app);
 
@@ -10,8 +10,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views'));
 
-var mysql		= require('mysql');
-var db			= require('./db');
+var mysql       = require('mysql');
+var db          = require('./db');
 
 app.get('/', function(req, res) {
     res.render('index2', {title: 'The index page!'})
@@ -20,18 +20,74 @@ app.get('/', function(req, res) {
 
 app.get('/result',function(req,res){
     var return_set  = [];
-    db.query('SELECT INSTNM, STABBR, TUITION1 FROM HigherEducation3 INNER JOIN CollegeTuitions ON HigherEducation3.UNITID=CollegeTuitions.UNITID LIMIT 5;', function(err, result) {
-    	for(var i=0; i < result.length; i++){
-    		return_set.push(
-    				{ 
-    					instnm: result[i].INSTNM, 
-    					pref_state: result[i].STABBR,
-    					tuition: result[i].TUITION1,
-    				});
-    	}
-    	res.render("result", {results: return_set})
-    	console.log({results: return_set});
-    	if (err) throw err;
+
+    var personality_type = req.body.personality;
+
+    if(personality_type == NULL){
+
+        var long_query = 'SELECT b.INSTNM, b.STABBR, c.TUITION1 as INSTATE, (c.TUITION3*1.5) AS OUTSTATE, (b.DIVISOR/a.MB_TYPE_SCORE) AS MATCH_RATE FROM Personality a, HigherEducation3 b, CollegeTuitions c WHERE a.MB_TYPE = "' + personality_type + '" AND (a.MB_TYPE_SCORE = b.P_SCORE) AND b.UNITID=c.UNITID GROUP BY b.INSTNM HAVING MATCH_RATE < 0.95 ORDER BY MATCH_RATE DESC LIMIT 5;';
+
+        db.query(long_query, function(err, result) {
+            for(var i=0; i < result.length; i++){
+                return_set.push(
+                        { 
+                            instnm: result[i].INSTNM, 
+                            pref_state: result[i].STABBR,
+                            instate: result[i].INSTATE,
+                            outstate: result[i].OUTSTATE,
+                        });
+            }
+            res.render("result", {results: return_set})
+            console.log({results: return_set});
+            if (err) throw err;
+        });
+
+
+    } else {
+
+        db.query('SELECT INSTNM, STABBR, TUITION1 AS INSTATE, (TUITION3*1.5) AS OUTSTATE FROM HigherEducation3 INNER JOIN CollegeTuitions ON HigherEducation3.UNITID=CollegeTuitions.UNITID LIMIT 5;', function(err, result) {
+            for(var i=0; i < result.length; i++){
+                return_set.push(
+                        { 
+                            instnm: result[i].INSTNM, 
+                            pref_state: result[i].STABBR,
+                            instate: result[i].INSTATE,
+                            outstate: result[i].OUTSTATE,
+                        });
+            }
+            res.render("result", {results: return_set})
+            console.log({results: return_set});
+            if (err) throw err;
+        });
+
+    }
+});
+
+
+
+
+
+app.get('/personality',function(req,res){
+    var return_set  = [];
+
+    var personality_type = 'ESFP';
+
+    var long_query = 'SELECT b.INSTNM, b.STABBR, c.TUITION1 as INSTATE, c.TUITION2 as OUTSTATE, (b.DIVISOR/a.MB_TYPE_SCORE) AS MATCH_RATE FROM Personality a, HigherEducation3 b, CollegeTuitions c WHERE a.MB_TYPE = "' + personality_type + '" AND (a.MB_TYPE_SCORE = b.P_SCORE) AND b.UNITID=c.UNITID GROUP BY b.INSTNM HAVING MATCH_RATE < 0.95 ORDER BY MATCH_RATE DESC LIMIT 5;';
+
+
+    db.query(long_query, function(err, result) {
+        for(var i=0; i < result.length; i++){
+            return_set.push(
+                    { 
+                        instnm: result[i].INSTNM, 
+                        pref_state: result[i].STABBR,
+                        instate: result[i].INSTATE,
+                        outstate: result[i].OUTSTATE,
+                    });
+        }
+        res.render("result", {results: return_set})
+        console.log({results: return_set});
+        if (err) throw err;
     });
 });
 
