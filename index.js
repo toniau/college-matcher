b45c7eb99467876ec3e79a4fcabbc9de1e992ebe
ubiquitous-kumquat <izzyjs@yahoo.com>
2016-12-06 16:10:30 -0500

    connected result ejs with backend

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ var mysql       = require('mysql');
 var db          = require('./db');
 
 app.get('/', function(req, res) {
-    res.render('index2', {title: 'The index page!'})
+    res.sendFile(path.join(__dirname+'index.html'));
 });
 
 
 app.get('/result',function(req,res){
     var return_set  = [];
 
-    var personality_type = req.body.personality;
+    var personality_type = req.query['personality'];
 
-    if(personality_type == NULL){
+    console.log("PERSONALITY TYPE:" + req.query['personality']);
+    console.log(typeof personality_type);
+    console.log(personality_type);
+
+    if(personality_type != ''){
 
         var long_query = 'SELECT b.INSTNM, b.STABBR, c.TUITION1 as INSTATE, (c.TUITION3*1.5) AS OUTSTATE, (b.DIVISOR/a.MB_TYPE_SCORE) AS MATCH_RATE FROM Personality a, HigherEducation3 b, CollegeTuitions c WHERE a.MB_TYPE = "' + personality_type + '" AND (a.MB_TYPE_SCORE = b.P_SCORE) AND b.UNITID=c.UNITID GROUP BY b.INSTNM HAVING MATCH_RATE < 0.95 ORDER BY MATCH_RATE DESC LIMIT 5;';
 
@@ -37,13 +41,15 @@ app.get('/result',function(req,res){
                             outstate: result[i].OUTSTATE,
                         });
             }
-            res.render("result", {results: return_set})
+            res.render("result", {results: return_set});
             console.log({results: return_set});
             if (err) throw err;
         });
 
 
     } else {
+
+        console.log("IT RAN THE ELSE STATEMENT");
 
         db.query('SELECT INSTNM, STABBR, TUITION1 AS INSTATE, (TUITION3*1.5) AS OUTSTATE FROM HigherEducation3 INNER JOIN CollegeTuitions ON HigherEducation3.UNITID=CollegeTuitions.UNITID LIMIT 5;', function(err, result) {
             for(var i=0; i < result.length; i++){
@@ -55,7 +61,8 @@ app.get('/result',function(req,res){
                             outstate: result[i].OUTSTATE,
                         });
             }
-            res.render("result", {results: return_set})
+            //res.render("result", {results: return_set});
+            res.render("result", {results: return_set});
             console.log({results: return_set});
             if (err) throw err;
         });
